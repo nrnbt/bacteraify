76c9b1979fcdb696cb5e8a00554034ded04dfd5f
nrnbt <nrnbt0616@gmail.com>
2023-11-26 19:02:26 +0800

    prepared for heruko

--- core/settings.py
+++ core/settings.py
@@ -193,7 +193,11 @@ STATICFILES_DIRS = [
 
 STATIC_URL = "/static/"
 STATIC_ROOT = BASE_DIR / "core" / "staticfiles"
-STATICFILES_STORAGE="whitenoise.storage.CompressedManifestStaticFilesStorage"
+STORAGES = {
+    "staticfiles": {
+        "BACKEND": "whitenoise.storage.CompressedManifestStaticFilesStorage",
+    },
+}
 
 MEDIA_URL = "media/"
 MEDIA_ROOT = BASE_DIR / "core" / "media"
