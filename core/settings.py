d88e888846dbc17a69640ef3854a03520a9519f7
nrnbt <nrnbt0616@gmail.com>
2023-11-26 17:21:31 +0800

    upd

--- core/settings.py
+++ core/settings.py
@@ -138,7 +138,7 @@ DATABASES = {
 DATABASES['default'] = dj_database_url.config(conn_max_age=600, ssl_require=True)
 DATABASES['default']['OPTIONS']['charset'] = 'utf8mb4'
 del DATABASES['default']['OPTIONS']['sslmode']
-# DATABASES['default']['OPTIONS']['ssl'] =  {'ca': os.environ.get('MYSQL_ATTR_SSL_CA')}
+DATABASES['default']['OPTIONS']['ssl'] =  {'ca': os.environ.get('MYSQL_ATTR_SSL_CA')}
 
 
 # Password validation
