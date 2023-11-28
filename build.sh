8df1d849db7b3768bfa69540c8d5b0126814df71
nrnbt <nrnbt0616@gmail.com>
2023-11-28 23:36:12 +0800

    build update

--- build.sh
+++ build.sh
@@ -1,5 +1,8 @@
 source venv/bin/activate
 
+echo "Installing requirements..."
+pip install -r requirements.txt
+
 echo "Collecting static files..."
 python manage.py collectstatic --noinput
 deactivate
